1edebae36d007362ec07d220a862b96f8e08b2a3
Habibamohammedd <habibamohammed5432@gmail.com>
2025-02-14 01:26:14 +0200

    Update main.js
    added movement of stars

--- main.js
+++ main.js
@@ -65,4 +65,85 @@ navLinks.forEach(link => {
 });
 
 
+//////////////////////////////////////stars///////////////////////////////////////
+
+var canvas, context;
+var screenH, screenW;
+var stars = [];
+var fps = 50;
+var numStars = 10; // Fewer stars for a cleaner effect
+
+$(document).ready(function () {
+  screenH = $(window).height();
+  screenW = $(window).width();
+
+  canvas = $('#space');
+  canvas.attr('height', screenH);
+  canvas.attr('width', screenW);
+  context = canvas[0].getContext('2d');
+
+  for (var i = 0; i < numStars; i++) {
+    var x = Math.random() * screenW;
+    var y = Math.random() * screenH;
+    var speed = 0.2 + Math.random() * 0.5; // Slower movement
+    var direction = Math.random() < 0.5 ? 1 : -1;
+    var length = 1 + Math.random() * 2;
+    var opacity = 0.5 + Math.random() * 0.5; // Brighter stars
+
+    stars.push(new Star(x, y, speed, direction, length, opacity));
+  }
+
+  setInterval(animate, 1000 / fps);
+});
+
+// Star constructor
+function Star(x, y, speed, direction, length, opacity) {
+  this.x = x;
+  this.y = y;
+  this.speedX = speed * direction; // Horizontal movement
+  this.speedY = speed * 0.3; // Slower vertical movement
+  this.length = length;
+  this.opacity = opacity;
+}
+
+Star.prototype.draw = function () {
+  context.save();
+  context.translate(this.x, this.y);
+
+  context.beginPath();
+  for (var i = 5; i--;) {
+    context.lineTo(0, this.length);
+    context.translate(0, this.length);
+    context.rotate(Math.PI * 2 / 10);
+    context.lineTo(0, -this.length);
+    context.translate(0, -this.length);
+    context.rotate(-Math.PI * 6 / 10);
+  }
+  context.closePath();
+
+  context.fillStyle = `rgba(255, 255, 200, ${this.opacity})`;
+  context.shadowBlur = 5;
+  context.shadowColor = '#ffffff';
+  context.fill();
+
+  context.restore();
+};
+
+// Update animation
+function animate() {
+  context.clearRect(0, 0, screenW, screenH);
+
+  stars.forEach(star => {
+    star.x += star.speedX; // Move stars horizontally
+    star.y += star.speedY; // Move stars slowly downward
+
+    // Reset position when stars leave the screen
+    if (star.x > screenW || star.y > screenH) {
+      star.x = Math.random() * screenW;
+      star.y = -10; // Restart from the top
+    }
+
+    star.draw(context);
+  });
+}
 
